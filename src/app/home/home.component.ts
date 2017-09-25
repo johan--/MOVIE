a89616365a6e1735ac1229ebf9f1e782aa486365
InApp <sindhu.jp@inapp.com>
2017-09-25 10:00:24 +0530

    movie files change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
 import {Movie} from '../movies';
+import { Router }            from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,18 +9,26 @@ import {Movie} from '../movies';
 })
 export class HomeComponent implements OnInit {
   movies:string;
-  constructor() { 
+  constructor(private router: Router) { 
     
   }
   getFavorite():void{
     //debugger;
-     //this.movies = window.localStorage.getItem('data');
+     //this.movies = window.localStorage.getItem('films');
      this.movies = JSON.parse(window.localStorage.getItem("data"));
      
      console.log(this.movies);
   }
-  gotoDis():void{   
-    window.localStorage.removeItem('data');   
+  gotoDis(idx:number):void{   
+    //debugger;
+    //console.log(idx);
+    var data=[];
+    data = JSON.parse(window.localStorage.getItem("data"));
+     data.splice(idx, 1);  
+     window.localStorage.setItem('data', JSON.stringify(data));
+     //this.router.navigate(['/home']);
+     window.location.reload();
+   // window.localStorage.removeItem('data');   
     
   }
   ngOnInit() {
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -24,9 +24,31 @@ export class SearchComponent implements OnInit {
 
   ngOnInit() {
     
+    /*var films = [
+      {
+       'id': 1,
+       'title':'John Wick',
+       'Genre':'Action'
+      },
+      {
+       'id': 2,
+       'title':'Taken 3',
+       'Genre':'Action'
+      },
+      {
+       'id': 3,
+       'title':'The Guard',
+       'Genre':'Comedy'
+      }
+     ];
+     localStorage.setItem("films", JSON.stringify(films));
+     var mn = localStorage.getItem("films");
+     mn     = JSON.parse(mn);
+     console.log(mn);
+*/
     this.route.paramMap
     .switchMap((params: ParamMap) => {
-      //console.log(this.movieService.getMovie(params.get('name')))
+      console.log(this.movieService.getMovie(params.get('name')))
       return this.movieService.getMovie(params.get('name'))
     })
     .subscribe(movies => 
@@ -39,6 +61,12 @@ export class SearchComponent implements OnInit {
   }
   
   gotoFav(movie: Movie): void {
-    window.localStorage.setItem('data',JSON.stringify(movie));
+    var data = [];
+    data = JSON.parse(window.localStorage.getItem("data"));
+    if(data==null) {
+      data = [];
+    }
+    data.push(movie);
+    window.localStorage.setItem('data', JSON.stringify(data));
   }
 }
